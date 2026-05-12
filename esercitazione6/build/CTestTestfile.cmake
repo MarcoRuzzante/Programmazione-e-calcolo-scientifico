6bc7c62e2776f5877f01145a1b2be8af2d526ae1
Marco Ruzzante <marco.ruzzante98@gmail.com>
2026-05-12 10:06:03 +0000

    esercitazione6: grafi

---
+++ esercitazione6/build/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: /home/pcsdocker/Data/Programmazione-e-calcolo-scientifico/esercitazione6
+# Build directory: /home/pcsdocker/Data/Programmazione-e-calcolo-scientifico/esercitazione6/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test([=[Edge_Unit_Test]=] "/home/pcsdocker/Data/Programmazione-e-calcolo-scientifico/esercitazione6/build/test_edge")
+set_tests_properties([=[Edge_Unit_Test]=] PROPERTIES  _BACKTRACE_TRIPLES "/home/pcsdocker/Data/Programmazione-e-calcolo-scientifico/esercitazione6/CMakeLists.txt;11;add_test;/home/pcsdocker/Data/Programmazione-e-calcolo-scientifico/esercitazione6/CMakeLists.txt;0;")
+add_test([=[Graph_Unit_Test]=] "/home/pcsdocker/Data/Programmazione-e-calcolo-scientifico/esercitazione6/build/test_graph")
+set_tests_properties([=[Graph_Unit_Test]=] PROPERTIES  _BACKTRACE_TRIPLES "/home/pcsdocker/Data/Programmazione-e-calcolo-scientifico/esercitazione6/CMakeLists.txt;14;add_test;/home/pcsdocker/Data/Programmazione-e-calcolo-scientifico/esercitazione6/CMakeLists.txt;0;")
---
+++ esercitazione6/test_edge.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include "graph.h"
+
+int main() {
+
+    undirected_edge<int> e1(5, 2);
+    
+    // Verifica vincolo (from < to)
+    if (e1.from() != 2 || e1.to() != 5) {
+        return EXIT_FAILURE;
+    }
+
+    undirected_edge<int> e2(2, 5);
+    
+    if (!(e1 == e2)) {
+        return EXIT_FAILURE;
+    }
+
+    if ((e1 < e2) || (e2 < e1)) {
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
+}
